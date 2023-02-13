a8bcae2fea083fb6f9a6cc780b2c5d31f9937902
raffaele.lubrano <raffaele.lubrano@InfoTele23.maxwell.local>
2023-02-13 10:57:23 +0100

    bar

---
+++ src/progbar/Coda.java
@@ -0,0 +1,43 @@
+
+package progbar;
+
+import java.util.Vector;
+
+
+class Coda{
+  private Vector elementi;
+
+  public Coda() {
+    elementi = new Vector();
+  }
+
+  public void aggiungi(Object obj) {
+    elementi.addElement(obj);
+  }
+
+  public Object togli() {
+    Object obj = null;
+    int size = elementi.size();
+
+    if (size > 0) {
+      obj = elementi.elementAt(0);
+      elementi.removeElementAt(0);
+    }
+    return obj;
+  }
+
+  public boolean vuota() {
+    if (elementi.size() > 0)
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+
+  public int size() {
+    return elementi.size();
+  }
+}
---
+++ src/progbar/Ordine.java
@@ -0,0 +1,20 @@
+
+package progbar;
+
+
+class Ordine {
+  private String tavolo;
+  private String ordine;
+  
+  public Ordine(String tav, String ord)
+  {
+    tavolo = tav;
+    ordine = ord;
+  }
+
+  public void stampa()
+  {
+    System.out.println("Tavolo: "+tavolo);
+    System.out.println("Ordine: "+ordine);
+  }
+}
